073a71a3cfe01456309a38b0041e41bdc40a0acc
Paul Gorman <gorman.paul@gmail.com>
2018-01-20 20:17:41 -0500

    Tweaked appearance, and added Advanced options.

--- spinmpc.go
+++ spinmpc.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"os/exec"
 	"path"
 	"strings"
 	"time"
@@ -20,19 +21,20 @@ var err error
 var debug *bool
 
 type Configuration struct {
-	Debug bool
+	Debug bool `json:"debug"`
 	MPD   struct {
-		Address  string
-		Port     string
-		Password string
-	}
+		Address  string `json:"address"`
+		Port     string `json:"port"`
+		Password string `json:"password"`
+		Kill     string `json:"kill"`
+	} `json:"mpd"`
 	Web struct {
-		Address  string
-		Port     string
-		Password string
-		Root     string
-		Search   string
-	}
+		Address  string `json:"address"`
+		Port     string `json:"port"`
+		Password string `json:"password"`
+		Root     string `json:"root"`
+		Search   string `json:"search"`
+	} `json:"web"`
 }
 
 // Configure selects settings from defaults, the config file, and command-line flags.
@@ -42,6 +44,7 @@ func Configure() Configuration {
 	mpdaddr := flag.String("mdpaddr", "", "Specify the address of the interface where MPD listens.")
 	mpdport := flag.String("mdpport", "", "Specify the port on which MPD listens.")
 	mpdpass := flag.String("mdppass", "", "Specify password required by MPD (if any).")
+	mpdkill := flag.String("mdpkill", "", "Specify the command to kill MPD.")
 	webaddr := flag.String("webaddr", "", "Specify the address of the interface where SpinMPC serves its web interface.")
 	webport := flag.String("webport", "", "Specify the port on which SpinMPC serves its web interface.")
 	webpass := flag.String("webpass", "", "Password to require for access to SpinMPC's web interface.")
@@ -54,6 +57,7 @@ func Configure() Configuration {
 	conf.MPD.Address = "127.0.0.1"
 	conf.MPD.Port = "6600"
 	conf.MPD.Password = ""
+	conf.MPD.Kill = "/usr/bin/mpd --kill"
 	conf.Web.Address = ""
 	conf.Web.Port = "8870"
 	conf.Web.Password = ""
@@ -82,6 +86,9 @@ func Configure() Configuration {
 	}
 	if *mpdpass != "" {
 		conf.MPD.Password = *mpdpass
+	}
+	if *mpdkill != "" {
+		conf.MPD.Password = *mpdkill
 	}
 	if *webaddr != "" {
 		conf.Web.Address = *webaddr
@@ -154,43 +161,45 @@ func ConnectMPD(conf *Configuration) *mpd.Client {
 }
 
 // FillPlaylist populates an empty default playlist with all files in the database.
-func FillPlaylist(conn *mpd.Client) {
+func FillPlaylist(conn *mpd.Client) error {
+	var err error
 	songs, err := conn.PlaylistInfo(-1, -1)
 	if err != nil {
-		log.Println("WARN: failed to get current playlist: ", err)
+		err = fmt.Errorf("WARN: failed to get current playlist: &v", err)
 	}
 	// Don't clobber the current playlist if it's already populated!
 	if len(songs) > 0 {
 		if *debug {
 			log.Println("INFO: Playlist already populated. Abandoning 'fillPlaylist'.")
 		}
-		return
+		return err
 	}
 
 	err = conn.Clear()
 	if err != nil {
-		log.Println("WARN: failed to clear playlist: ", err)
+		err = fmt.Errorf("WARN: failed to clear playlist: %v", err)
 	}
 	songs, err = conn.ListAllInfo("/")
 	if err != nil {
-		log.Println(err)
+		err = fmt.Errorf("WARN: failed to get song info: %v", err)
 	}
 	for _, s := range songs {
 		err = conn.Add(s["file"])
 		if err != nil {
-			log.Println("WARN: can't add file to playlist: ", err)
+			err = fmt.Errorf("WARN: can't add file to playlist: %v", err)
 		}
 	}
 
 	// Start and immiately stop, so there's a song populated in "status". Why is this needed?
 	err = conn.Play(-1)
 	if err != nil {
-		log.Println("WARN: failed to stop playback: ", err)
+		err = fmt.Errorf("WARN: failed to stop playback: %v", err)
 	}
 	err = conn.Stop()
 	if err != nil {
-		log.Println("WARN: failed to stop playback: ", err)
+		fmt.Errorf("WARN: failed to stop playback: %v", err)
 	}
+	return err
 }
 
 // KeepAlive keeps our connection to MPD open.
@@ -253,10 +262,28 @@ func main() {
 	conn = &c
 	defer (*conn).Close()
 	go KeepAlive(conn, &conf)
-	FillPlaylist(*conn)
+	err = FillPlaylist(*conn)
+	if err != nil {
+		log.Println(err)
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, path.Join(conf.Web.Root, "index.html"))
+	})
+
+	http.HandleFunc("/api/v1/allsongs", func(w http.ResponseWriter, r *http.Request) {
+		err := (*conn).Clear()
+		if err != nil {
+			log.Println("WARN: failed to clear play queue: ", err)
+		}
+		FillPlaylist(*conn)
+	})
+
+	http.HandleFunc("/api/v1/clearqueue", func(w http.ResponseWriter, r *http.Request) {
+		err := (*conn).Clear()
+		if err != nil {
+			log.Println("WARN: failed to clear play queue: ", err)
+		}
 	})
 
 	http.HandleFunc("/api/v1/currentsong", func(w http.ResponseWriter, r *http.Request) {
@@ -268,12 +295,22 @@ func main() {
 		json.NewEncoder(w).Encode(s)
 	})
 
-	http.HandleFunc("/api/v1/playlists", func(w http.ResponseWriter, r *http.Request) {
-		playlists, err := Playlists(*conn)
-		if err != nil {
-			log.Println(err)
+	http.HandleFunc("/api/v1/killmpd", func(w http.ResponseWriter, r *http.Request) {
+		if conf.Debug {
+			log.Println("INFO: Received API call for 'killmpd'.")
 		}
-		json.NewEncoder(w).Encode(playlists)
+		// This could probably be done more safely:
+		cmd := exec.Command(strings.Fields(conf.MPD.Kill)[0], strings.Fields(conf.MPD.Kill)[1:]...)
+		err := cmd.Run()
+		if err != nil {
+			log.Println("WARN: problem killing MPD: ", err)
+		}
+	})
+
+	http.HandleFunc("/api/v1/killspinmpc", func(w http.ResponseWriter, r *http.Request) {
+		if conf.Debug {
+			log.Fatal("Exiting because of API call to 'killspinmpc'. Bye.")
+		}
 	})
 
 	http.HandleFunc("/api/v1/next", func(w http.ResponseWriter, r *http.Request) {
@@ -295,6 +332,14 @@ func main() {
 		if err != nil {
 			log.Println("WARN: failed to play: ", err)
 		}
+	})
+
+	http.HandleFunc("/api/v1/playlists", func(w http.ResponseWriter, r *http.Request) {
+		playlists, err := Playlists(*conn)
+		if err != nil {
+			log.Println(err)
+		}
+		json.NewEncoder(w).Encode(playlists)
 	})
 
 	http.HandleFunc("/api/v1/previous", func(w http.ResponseWriter, r *http.Request) {
@@ -338,6 +383,13 @@ func main() {
 		}
 		defer r.Body.Close()
 		log.Println(t.Playlist)
+	})
+
+	http.HandleFunc("/api/v1/updatempdatabase", func(w http.ResponseWriter, r *http.Request) {
+		_, err = (*conn).Update("")
+		if err != nil {
+			log.Println("WARN: failed to update MPD database: ", err)
+		}
 	})
 
 	log.Fatal(http.ListenAndServe(conf.Web.Address+":"+conf.Web.Port, nil))
